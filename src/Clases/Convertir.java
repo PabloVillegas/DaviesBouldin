9c5536e727b9aa2ebe05becaf175e23a19e1e203
PabloVillegas <pablovillegaz@gmail.com>
2015-11-04 20:30:26 -0600

    agregue convertir

--- src/Clases/Convertir.java
+++ src/Clases/Convertir.java
@@ -10,5 +10,21 @@ package Clases;
  * @author v
  */
 public class Convertir {
+
+    public Convertir() {
+    }
     
+    public Double[][] convertirObjetos(String[][] objetos){
+        Double[][] objetosD = new Double[objetos.length][objetos[0].length];
+        for (int i = 0; i < objetos.length; i++) {
+            for (int j = 0; j < objetos[i].length; j++) {
+                if(objetos[i][j]==null){
+                    objetosD[i][j] = null;
+                }else{
+                    objetosD[i][j] = objetos[i][j];
+                }
+            }
+        }
+        return objetosD;
+    }
 }
